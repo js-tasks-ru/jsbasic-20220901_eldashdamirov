8b26c0949d5e2cfa451897203657bab600909d94
Ellbrus <94366401+Ellbrus@users.noreply.github.com>
2022-09-08 16:36:02 +0300

    Update index.js

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,8 +1,18 @@
-function factorial(n) {
-  if (n === 0){
-    return 1;
-  }else{
-    return n * factorial(n-1)
-  }
-}
+// function factorial(n) {
+//   if (n === 0){
+//     return 1;
+//   }else{
+//     return n * factorial(n-1)
+//   }
+// }
 
+ function factorial(n) {
+    let result = 1
+  
+    for (let i = 0; i < n; i++) {
+      result *= i + 1
+    }
+    return result
+  }
+  
+  console.log(factorial(1))
